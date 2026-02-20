870bc3569d55ca342245b0ac17ea4de17b26b413
yusufguc <gucyusuff@gmail.com>
2026-02-20 21:30:04 +0300

    feat(auth): update JwtAuthenticationFilter

--- src/main/java/com/yusufguc/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/yusufguc/jwt/JwtAuthenticationFilter.java
@@ -22,8 +22,8 @@ import java.io.IOException;
 @Log4j2
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private  JwtService jwtService;
-    private UserDetailsService userDetailsService;
+    private  final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
